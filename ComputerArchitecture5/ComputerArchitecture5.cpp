fe3427296ef9f3791ac5d33ab39d00a25b1d5e23
delokoseni <107359196+delokoseni@users.noreply.github.com>
2023-04-09 15:04:23 +0700

    Не работает

--- ComputerArchitecture5/ComputerArchitecture5.cpp
+++ ComputerArchitecture5/ComputerArchitecture5.cpp
@@ -5,12 +5,11 @@
 #include <chrono>
 using namespace std;
 
-const int N = 100;
+const int N = 300;
 
 int main(int *argc, char** argv) {
-	int numtasks, rank;
 	srand(time(NULL));
-	int arr[N][N], i, j, indi, indj, difference;
+	int arr[N][N], i, j, indi, indj, difference, numtasks, rank, res1, res2;
 	float averagevalue;
 	for (i = 0; i < N; i++)
 		for (j = 0; j < N; j++)
@@ -18,21 +17,25 @@ int main(int *argc, char** argv) {
 	MPI_Init(argc, &argv);
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 	MPI_Comm_size(MPI_COMM_WORLD, &numtasks);
-	cout << "Hello MPI from prouses: " << rank << ", total number of prouseses: " << numtasks << endl;
 	MPI_Barrier(MPI_COMM_WORLD);
 	MPI_Bcast(arr, N * N, MPI_INT, 0, MPI_COMM_WORLD);
+	int begin, end;
 	auto start_time = MPI_Wtime();
+	begin = rank * (N / numtasks); //номер процесса умножается на отношение размерности массива к количеству процессов
+	end = begin + (N / numtasks);  //к началу приавляется отношение размерности массива к количеству процессов
 	averagevalue = arr[0][0];
-	for (i = 0; i < N; i++)
+	//MPI_Bcast(&averagevalue, 1, MPI_SUM, 0, MPI_COMM_WORLD);
+	for (i = begin; i < end; i++)
 		for (j = 1; j < N; j++)
 			averagevalue += arr[i][j];
 	averagevalue /= N * N;
-	MPI_Bcast(&averagevalue, 1, MPI_FLOAT, 0, MPI_COMM_WORLD);
 	difference = abs(arr[0][0] - averagevalue);
-	MPI_Bcast(&difference, 1, MPI_INT, 0, MPI_COMM_WORLD);
+	//MPI_Bcast(&difference, 1, MPI_INT, 0, MPI_COMM_WORLD);
 	indi = 0;
+	//MPI_Bcast(&indi, 1, MPI_INT, 0, MPI_COMM_WORLD);
 	indj = 0;
-	for (i = 0; i < N; i++)
+	//MPI_Bcast(&indj, 1, MPI_INT, 0, MPI_COMM_WORLD);
+	for (i = begin; i < end; i++)
 		for (j = 1; j < N; j++) {
 			if (abs(arr[i][j] - averagevalue) < difference) {
 				indi = i;
@@ -40,12 +43,13 @@ int main(int *argc, char** argv) {
 				difference = abs(arr[i][j] - averagevalue);
 			}
 		}
+	//MPI_Reduce(&res1, &res2, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
 	auto end_time = MPI_Wtime();
-	MPI_Finalize();
 	cout << "The coordinates of the element closest to the average of all array elements:" << indi << ", " << indj << endl;
 	cout << "Average value of all array elements:" << averagevalue << endl;
 	cout << "The element closest to it: " << arr[indi][indj] << endl;
 	cout << "Execution time of the main algorithm of the program:" << end_time - start_time << endl;
+	MPI_Finalize();
 	return 0;
 }
 
